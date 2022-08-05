708accd952e1bfaf798dd9193f43517ce3e2ee2c
Atanas Irikev <atanas.irikev@gmail.com>
2022-08-05 15:31:33 +0300

    Project and client queries

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const connectDB = require('./config/db')
+
+const PORT = process.env.PORT || 5000
+
+// Connect to database
+connectDB()
+
+const app = express()
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+)
+
+app.listen(PORT, console.log(`Server runing on port: ${PORT}`))
